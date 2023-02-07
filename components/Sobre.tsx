cca4cbcec3b4bf6f32e283c839ce108bdb8cf914
Enzo <enzolozano90@gmail.com>
2023-02-07 12:50:50 -0300

    paragrafo alinhado na direita

--- components/Sobre.tsx
+++ components/Sobre.tsx
@@ -1,21 +1,27 @@
 import styles from "../styles/sobre.module.css"
 export default function Sobre() {
   return (
-    <section>
-      <h1 className={styles.SobreNos}>
-        Sobre Nós
-      </h1>
-      <p>Nossa visão é construir uma <br></br> solução empresarial de sucesso.</p>
-      <p>Um processo de desenvolvimento bem estruturado é crucial para criar<br></br> produtos
-        que as pessoas adoram. CEOS tem um processo de <br></br> desenvolvimento rigoroso que inclui o desenvolvimento
-        dirigido por<br></br> testes.
+    <section className={styles.section}>
+      <h1 className={styles.SobreNos}>Sobre Nós</h1>
+      <p className={styles.titulo}>
+        Nossa visão é construir uma <br></br> solução empresarial de sucesso.
       </p>
-      <p>4500</p>
-      <p>250+</p>
-      <p>350</p>
-      <p>Project done</p>
-      <p>Clients</p>
-      <p>Running Work</p>
+      <p className={styles.paragrafo}>
+        Um processo de desenvolvimento bem estruturado é crucial para criar
+        <br></br> produtos que as pessoas adoram. CEOS tem um processo de{" "}
+        <br></br> desenvolvimento rigoroso que inclui o desenvolvimento dirigido
+        por<br></br> testes.
+      </p>
+      <span className={styles.numeros}>
+        <p className={styles.n1}>4500</p>
+        <p className={styles.n2}>250+</p>
+        <p>350</p>
+      </span>
+      <span className={styles.descricao}>
+        <p className={styles.p1}>Project done</p>
+        <p className={styles.p2}>Clients</p>
+        <p className={styles.p3}>Running Work</p>
+      </span>
     </section>
   )
 }
