5842b6213276369757b9feecaf67f217ca3cbd47
Enzo <enzolozano90@gmail.com>
2023-02-07 12:13:00 -0300

    texto inserido

---
+++ components/Sobre.tsx
@@ -0,0 +1,21 @@
+import styles from "../styles/sobre.module.css"
+export default function Sobre() {
+  return (
+    <section>
+      <h1 className={styles.SobreNos}>
+        Sobre Nós
+      </h1>
+      <p>Nossa visão é construir uma <br></br> solução empresarial de sucesso.</p>
+      <p>Um processo de desenvolvimento bem estruturado é crucial para criar<br></br> produtos
+        que as pessoas adoram. CEOS tem um processo de <br></br> desenvolvimento rigoroso que inclui o desenvolvimento
+        dirigido por<br></br> testes.
+      </p>
+      <p>4500</p>
+      <p>250+</p>
+      <p>350</p>
+      <p>Project done</p>
+      <p>Clients</p>
+      <p>Running Work</p>
+    </section>
+  )
+}
