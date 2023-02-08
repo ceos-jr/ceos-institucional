15c20416c723134815aa5640f88f899b3088ea2e
Luiz Gustavo <gustavo1204.gc@gmail.com>
2023-02-07 21:02:28 -0300

    Add carouselSlide component

---
+++ components/Carousel/CarouselSlide.tsx
@@ -0,0 +1,24 @@
+import { ReactNode, CSSProperties } from "react";
+
+interface CarouselSlideProps {
+  children: ReactNode;
+  position: number;
+  className?: string;
+}
+
+const CarouselSlide = ({ children, position, className }: CarouselSlideProps) => {
+  const itemStyle = {
+    transform: `translateX(${position * 100}%)`,
+  } as CSSProperties;
+
+  return (
+    <div
+      className={`absolute h-full w-full ${className}`}
+      style={itemStyle}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default CarouselSlide;
