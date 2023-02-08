cac8a333f82a8f1b0b1c2d3aff0e62fe0d6913b4
Enzo <enzolozano90@gmail.com>
2023-02-08 18:04:46 -0300

    Imagem inserida, problemas com tamanho a alinhamento

--- components/Sobre.tsx
+++ components/Sobre.tsx
@@ -1,14 +1,24 @@
+import Image from "next/image"
 import styles from "../styles/sobre.module.css"
 export default function Sobre() {
   return (
     <section className={styles.section}>
-      <h1 className={styles.SobreNos}>Sobre Nós</h1>
+      <Image className={styles.ImagemSobre}
+      src="/ImagemSobre.png"
+      alt="Imagem"
+      width={677}
+      height={775}
+   
+      />
+      
+      
+      <h1 className={styles.SobreNos}>Sobre nós</h1>
       <p className={styles.titulo}>
         Nossa visão é construir uma <br></br> solução empresarial de sucesso.
       </p>
       <p className={styles.paragrafo}>
         Um processo de desenvolvimento bem estruturado é crucial para criar
-        <br></br> produtos que as pessoas adoram. CEOS tem um processo de{" "}
+        <br></br> produtos que as pessoas adoram. CEOS tem um processo de
         <br></br> desenvolvimento rigoroso que inclui o desenvolvimento dirigido
         por<br></br> testes.
       </p>
