252ff3b79b77ad6d4dabbacf2c0d8f3b5a0c0186
Luiz Gustavo <gustavo1204.gc@gmail.com>
2023-02-08 19:43:42 -0300

    Add support to multi slides to Carousel

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,13 +1,16 @@
 import { ReactNode, Children, CSSProperties, useState, TouchEvent } from "react"
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from "react-icons/hi"
 
 interface CarouselProps {
   children: ReactNode
   className?: string
+  show?: number
 }
 
 interface WrapperProps {
   children: ReactNode
   currentItem: number
+  show: number
   handleTouchStart: (e: TouchEvent) => void
   handleTouchMove: (e: TouchEvent) => void
 }
@@ -17,11 +20,12 @@ const SWIPE_THRESHOLD = 5
 const Wrapper = ({
   children,
   currentItem,
+  show,
   handleTouchStart,
   handleTouchMove,
 }: WrapperProps) => {
   const wrapperStyle = {
-    transform: `translateX(-${currentItem * 100}%)`,
+    transform: `translateX(-${currentItem * (100 / show)}%)`,
   } as CSSProperties
   return (
     <div
@@ -35,18 +39,18 @@ const Wrapper = ({
   )
 }
 
-const Carousel = ({ children, className }: CarouselProps) => {
+const Carousel = ({ children, className, show = 1 }: CarouselProps) => {
   const [activeItem, setActiveItem] = useState(0)
   const [touchPosition, setTouchPosition] = useState<number | null>(null)
   const NUM_OF_ITEMS = Children.toArray(children).length
 
   const moveToNext = () => {
-    if (activeItem === NUM_OF_ITEMS - 1) return
+    if (activeItem >= NUM_OF_ITEMS - show) return
 
     // Value must only range from 0 to `NUM_OF_ITEMS - 1`
     setActiveItem((prev) => {
-      return (prev + 1) % NUM_OF_ITEMS; 
-    });
+      return (prev + 1) % NUM_OF_ITEMS
+    })
   }
 
   const moveToPrev = () => {
@@ -54,8 +58,8 @@ const Carousel = ({ children, className }: CarouselProps) => {
 
     // Turn prev into a positive number in case it is negative
     setActiveItem((prev) => {
-      return (prev < 0 ? -prev : prev) - 1; 
-    });
+      return (prev < 0 ? -prev : prev) - 1
+    })
   }
 
   const handleTouchStart = (e: TouchEvent) => {
@@ -66,9 +70,9 @@ const Carousel = ({ children, className }: CarouselProps) => {
 
   const handleTouchMove = (e: TouchEvent) => {
     const touchDown = touchPosition
-    
+
     if (touchDown === null) return
-        
+
     const currentTouch = e.touches[0].clientX
     const difference = touchDown - currentTouch
 
@@ -84,14 +88,37 @@ const Carousel = ({ children, className }: CarouselProps) => {
   }
 
   return (
-    <div className={`relative h-96 w-full overflow-hidden ${className}`}>
+    <div className={`relative h-[26rem] w-full overflow-hidden ${className}`}>
       <Wrapper
+        show={show}
         currentItem={activeItem}
         handleTouchStart={handleTouchStart}
         handleTouchMove={handleTouchMove}
       >
         {children}
       </Wrapper>
+      <button
+        onClick={moveToPrev}
+        className={`
+          hidden md:block absolute outline-0 text-xl text-primary bottom-0 left-[45%] 
+          [transition:_color_300ms_ease-out_transform_300ms_ease-out] 
+          hover:text-accent hover:scale-[1.3] 
+          focus:text-accent focus:scale-[1.3]
+        `}
+      >
+        <HiOutlineChevronLeft />
+      </button>
+      <button
+        onClick={moveToNext}
+        className={`
+          hidden md:block absolute outline-0 text-xl text-primary bottom-0 right-[45%] 
+          [transition:_color_300ms_ease-out_transform_300ms_ease-out]
+          hover:text-accent hover:scale-[1.3] 
+          focus:text-accent focus:scale-[1.3]
+        `}
+      >
+        <HiOutlineChevronRight />
+      </button>
     </div>
   )
 }
