fe5ee6f761ca99881f02b0ca3ff40f4b92dafc8c
Jonathan Aires <airesjonathan123@gmail.com>
2023-02-09 12:47:28 -0300

    navbar começo

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import styles from "../styles/Navbar.module.css"
+import Image from "next/image"
+
+const Navbar = () => {
+  return (
+    <nav className={styles.Navbar}>
+      <Image src={"/Navbar/Ceos.svg"} alt="" width={120} height={120} />
+      <ul className={styles.Menu}>
+        <li>
+          <a href="#">Início</a>
+        </li>
+        <li>
+          <a href="#">Sobre nós</a>
+        </li>
+        <li>
+          <a href="#">Portfólio</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+      </ul>
+      <button className={styles.Button}>
+        <a href="#" rel="">
+          Fale conosco
+        </a>
+      </button>
+    </nav>
+  )
+}
+
+export default Navbar
