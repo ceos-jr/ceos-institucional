ea74cb544530644b1468f071f0d54ac9c66d0ffa
Enzo <enzolozano90@gmail.com>
2023-02-10 17:09:34 -0300

    ADICIONAR SUPORTE A TABLET

--- components/Sobre.tsx
+++ components/Sobre.tsx
@@ -9,24 +9,31 @@ export default function Sobre() {
       <div className={styles.div2}>
         <h1 className={styles.SobreNos}>Sobre nós</h1>
         <p className={styles.titulo}>
-          Nossa visão é construir uma <br></br> solução empresarial de sucesso.
+          Nossa visão é construir uma solução empresarial de sucesso.
         </p>
         <p className={styles.paragrafo}>
           Um processo de desenvolvimento bem estruturado é crucial para criar
-          <br></br> produtos que as pessoas adoram. CEOS tem um processo de
-          <br></br> desenvolvimento rigoroso que inclui o desenvolvimento
-          dirigido por<br></br> testes.
+          produtos que as pessoas adoram. CEOS tem um processo de
+          desenvolvimento rigoroso que inclui o desenvolvimento dirigido por
+          testes.
         </p>
-        <span className={styles.numeros}>
-          <p className={styles.n1}>4500</p>
-          <p className={styles.n2}>250+</p>
-          <p>350</p>
-        </span>
-        <span className={styles.descricao}>
-          <p className={styles.p1}>Project done</p>
-          <p className={styles.p2}>Clients</p>
-          <p className={styles.p3}>Running Work</p>
-        </span>
+        
+        
+        <div className={styles.descricao}>
+          <div>
+            <span>4500</span>
+            <h3>Project done</h3>
+          </div>
+          <div>
+            <span>250+</span>
+            <h3>Clients</h3>
+          </div>
+          <div>
+            <span>350</span>
+            <h3>Running work</h3>
+            
+          </div>
+        </div>
       </div>
     </section>
   )
