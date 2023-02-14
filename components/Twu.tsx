63512eeb5b300bc40d4ae8fa9d941d1a481ee31a
alanz <alanzcc1@gmail.com>
2023-02-14 16:36:48 -0300

    ajeitando o texto

--- components/Twu.tsx
+++ components/Twu.tsx
@@ -3,12 +3,24 @@ import Link from "next/link"
 import Image from "next/image"
 import styles from "../styles/twu.module.css"
 
-
 export default function Twu() {
   return (
-    <div>
-      <Image src="images/twu.svg" alt="twuimage" height={100} width={100}
-      className={styles.container} />
+    <div className={styles.global}>
+      <Image
+        src="images/twu.svg"
+        alt="twuimage"
+        height={100}
+        width={100}
+        className={styles.svgtwu}
+      />
+      <div className={styles.globaltext}>
+        <h1>Fale Conosco</h1>
+        <h2>Consulte gratuitamente nossos sobre seu projeto</h2>
+        <p>
+          Ajudamos sua empresa a refletir seu verdadeiro eu por meio de projetos
+          inovadores...
+        </p> 
+      </div>
     </div>
   )
 }
