0293ea49f365b5090f98540f73eb24ca558f4f36
Enzo <enzolozano90@gmail.com>
2023-02-15 11:28:53 -0300

    testando

--- components/Services.tsx
+++ components/Services.tsx
@@ -3,15 +3,12 @@ export default function Services() {
     return (
         <section>
              <div>
-                <h1>Serviços</h1>
-                <h1>Somos uma pequena equipe de contadores de histórias, pensadores e solucionadores de problemas.</h1>
-                <p>Ajudamos a sua empresa a refletir seu verdadeiro eu por meio de projetos inovadores</p>
+                <h1 className={styles.services}>Serviços</h1>
+                <h1 className={styles.titulo}>Somos uma pequena equipe de contadores de histórias, pensadores e solucionadores de problemas.</h1>
+                <p className={styles.paragrafo}>Ajudamos a sua empresa a refletir seu verdadeiro eu por meio de projetos inovadores</p>
              </div>
              <div className={styles.div2}>
-                <span className={styles.block4}>bloco1</span>
-                <span className={styles.block4}>bloco2</span>
-                <span className={styles.block4}>bloco3</span>
-                <span className={styles.block4}>bloco4</span>
+                
              </div>
 
         </section>
