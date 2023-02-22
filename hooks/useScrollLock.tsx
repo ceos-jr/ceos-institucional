bf3c1ff0a88c76cd0e519f0c08b99acbe293a86f
taldoflemis <coderflemis@gmail.com>
2023-02-22 13:55:32 -0300

    add comments to useScrollLock

--- hooks/useScrollLock.tsx
+++ hooks/useScrollLock.tsx
@@ -9,6 +9,9 @@ interface ScrollLockReturn {
 const useScrollLock = (): ScrollLockReturn => {
   const [scrollBarCompensation, setScrollBarComprensation] = useState<number>(0)
 
+  /* Aqui é onde a mágica acontece
+   * Calculamos a distancia entre a largura da tela inteira contra o tamanho do body sem a scrollbar
+   * E setamos o valor do scrollBarCompensation */
   const lockScroll = () => {
     setScrollBarComprensation(window.innerWidth - document.body.offsetWidth)
   }
@@ -17,6 +20,13 @@ const useScrollLock = (): ScrollLockReturn => {
     setScrollBarComprensation(0)
   }
 
+  /* Necessario para quando o NextJS fizer build
+   * A window não estará disponivel
+   * Se o tamanho da scrollbar for maior que 0, quer dizer que ela está visivel
+   * E setamos o overflow do body pra hidden
+   * Se não, setamos o overflow pra auto
+   * E sempre ajustamos o valor do paddingRight para compensar a scrollbar
+   * */
   if (typeof window !== "undefined") {
     document.body.style.overflow = scrollBarCompensation > 0 ? "hidden" : "auto"
     document.body.style.paddingRight = `${scrollBarCompensation}px`
